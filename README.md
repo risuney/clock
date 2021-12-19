152132ec81de7144b600e836496047bee0298ea3
risuney <honey197692@gmail.com>
2021-12-19 19:08:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple Clock
+![Simple Clock](https://risuney.github.io/clock/ogp.png)
+
+A so simple digital clock.
+
+Accuracy depends on your device's clock
+
+# How to Use
+
+Just [open the page](https://risuney.github.io/clock/).
+No configuration is required.
---
+++ index.js
@@ -0,0 +1,29 @@
+const road_date = String(Date.now())
+const road_datecut = Number(road_date.slice(0, -3))
+const setdate = Number(String(road_datecut + 2) + '00')
+
+const interval = setInterval(function(){
+  const now_date = String(Date.now())
+  const now_datecut = Number(now_date.slice(0, -1))
+  if (now_datecut == setdate) {
+    displayClock()
+    clearInterval(interval)
+  }
+}, 10)
+
+function displayClock() {
+  setInterval(function(){
+    const date = new Date()
+    const hh = zeroPad(date.getHours())
+    const mm = zeroPad(date.getMinutes())
+    const ss = zeroPad(date.getSeconds())
+    const time = `${hh}:${mm}:${ss}`
+    document.querySelector('#clock').textContent = time
+  },1000)
+}
+
+function zeroPad(num) {
+  const str = String(num)
+  const pad = str.padStart(2, 0)
+  return pad
+}
