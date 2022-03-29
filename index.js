52103e04794454db79ec1735c04324c93455f538
risuney <honey197692@gmail.com>
2022-03-30 01:04:36 +0900

    スパゲティをお直し

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const road_date = String(Date.now())
-const road_datecut = Number(road_date.slice(0, -3))
-const setdate = Number(String(road_datecut + 2) + '00')
+const road_date = String(Date.now()) //文字のDate
+const road_date_num = Number(road_date) //上をナンバーに変換
+const road_datecut = Number(road_date.slice(0, -3)) //msをsに変換（1000 -> 1.000  小数点以下切り捨て）
+const setdate = Number(String(road_datecut + 1) + '000') //road_datecutに1秒足してmsに変換
 
-const interval = setInterval(function(){
-  const now_date = String(Date.now())
-  const now_datecut = Number(now_date.slice(0, -1))
-  if (now_datecut == setdate) {
-    displayClock()
-    clearInterval(interval)
-  }
-}, 10)
+const time_difference = setdate - road_date_num //目標値(setdate)までのmsを算出
+
+console.log([road_date, road_date_num, road_datecut, setdate, time_difference])
+
+setTimeout(() => {
+  displayClock()
+}, time_difference)
 
 function displayClock() {
   setInterval(function(){
@@ -19,8 +19,9 @@ function displayClock() {
     const ss = zeroPad(date.getSeconds())
     const time = `${hh}:${mm}:${ss}`
     document.querySelector('#clock').textContent = time
-    if ((mm + ss) == '0000') {
-      location.reload();
+    const mmss = Number(mm + ss)
+    if (mmss % 500 == 0) {
+      location.reload() //5分毎にリロード
     }
   },1000)
 }
